52045cfb22fdaa9191fce96671fccf6a0efbcbf8
Marion <m.t.touja@gmail.com>
2018-12-20 12:32:36 +0100

    test

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,14 +1,9 @@
-<template>
-  <draggable v-model="myArray" :options="{group:'people'}" @start="drag=true" @end="drag=false" id="prout">
-    <div class="colonne" v-for="element in myArray" :key="element.id">{{element.name}}</div>
-    <div class="div2" style="height: 100px"></div>
-    <div class="div3" style="height: 100px"></div>
-  </draggable>
-</template>
+
+
 
 <script lang="ts">
 import Vue from 'vue'
-import draggable from 'vuedraggable'
+import draggable from 'vuedraggable';
 
 export default Vue.extend({
   components: {
@@ -17,33 +12,35 @@ export default Vue.extend({
   data: function() {
     return{
       myArray: [
-        {id: 1, name: 'couches'},
-        {id: 2, name: 'pot de confiture'},
-        {id: 3, name: "coquilles d'oeufs"},
-        {id: 4, name: 'boîtes de conserve'},
-        {id: 5, name: 'marc de café'}
-      ]
+        {id: 1, name: 'couches', order: 1,},
+        {id: 2, name: 'pot de confiture',order: 2},
+        {id: 3, name: "coquilles d'oeufs",order: 3},
+        {id: 4, name: 'boîtes de conserve', order: 4},
+        {id: 5, name: 'marc de café',order: 5}
+      ],
+      mySecondArray: [{ id: 6, name: 'bravo'}]
     }
-  }
+  },
+
 })
 </script>
 
 
 <style>
-  .juma {
-    background-color: aqua;
-  }
   h2 {
     color: white;
   }
   #prout {
     background-color: darkturquoise;
+    height: 400px;
   }
   #prout.drop_hover {
     box-shadow: 0 0 30px rgba(0,0,0,0.8) inset;
   }
   .colonne {
-    color: yellow;
+    color: black;
+     border: solid 3px;
+     font-size: 30px;
   }
   .div2 {
     background-color: red;
