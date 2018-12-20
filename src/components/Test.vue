7bfe84c5a756649a7dfd9239d22ae81bae7e2140
Marion <m.t.touja@gmail.com>
2018-12-20 13:52:51 +0100

    job fait sur le pc de tiphaine

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,9 +1,30 @@
+<template>
+ <draggable>
+  <template>
+    <draggable v-model="myArray" :options="{group:'people'}" @start="drag=true" @end="drag=false" id="listItem">
+     <div class="colonne" v-for="element in myArray" :key="element.id">{{element.name}}</div>
+    </draggable>  
+  </template> 
+   <template>
+     <draggable v-model="mySecondArray" :options="{group:'people'}" @start="drag=true" @end="drag=true" id="compost">
+      <div class="colonne" v-for="element in mySecondArray" :key="element.id">{{element.name}}</div>
+     </draggable>
+   </template>  
+   <template>
+     <draggable v-model="myThirdArray" :options="{group:'people'}" @start="drag=true" @end="drag=true" id="plastique">
+       <div class="colonne" v-for="element in myThirdArray" :key="element.id">{{element.name}}</div>
+     </draggable>
+   </template>
+  </draggable>
+</template>  
+
+
 
 
 
 <script lang="ts">
 import Vue from 'vue'
-import draggable from 'vuedraggable';
+import draggable from 'vuedraggable'
 
 export default Vue.extend({
   components: {
@@ -12,42 +33,56 @@ export default Vue.extend({
   data: function() {
     return{
       myArray: [
-        {id: 1, name: 'couches', order: 1,},
-        {id: 2, name: 'pot de confiture',order: 2},
-        {id: 3, name: "coquilles d'oeufs",order: 3},
-        {id: 4, name: 'boîtes de conserve', order: 4},
-        {id: 5, name: 'marc de café',order: 5}
+        {id: 1, name: 'couches'},
+        {id: 2, name: 'pot de confiture'},
+        {id: 3, name: "coquilles d'oeufs"},
+        {id: 4, name: 'boîtes de conserve'},
+        {id: 5, name: 'marc de café'}
       ],
-      mySecondArray: [{ id: 6, name: 'bravo'}]
+      mySecondArray: [
+        {id: 6, name: 'compost'},
+        {id: 7, name: 'carotte'},
+        {id: 8, name: "yaourt"},
+        {id: 9, name: 'carton'},
+        {id: 10, name: 'pommes'}
+      ],
+      myThirdArray: [
+        {id: 11, name: 'banane'},
+        {id: 12, name: 'carotte'},
+        {id: 13, name: "yaourt"},
+        {id: 14, name: 'carton'},
+        {id: 15, name: 'pommes'}
+      ]
     }
-  },
 
+  },
+  methods:{
+    
+  }
 })
 </script>
 
 
+
+
 <style>
+  
   h2 {
     color: white;
   }
-  #prout {
+  #listItem {
     background-color: darkturquoise;
-    height: 400px;
+    height: 190px;
   }
-  #prout.drop_hover {
-    box-shadow: 0 0 30px rgba(0,0,0,0.8) inset;
+  #compost{
+    background-color: maroon;
+    height: 190px;
   }
-  .colonne {
-    color: black;
-     border: solid 3px;
-     font-size: 30px;
+  #plastique{
+    background-color: purple;
+    height: 190px;
   }
-  .div2 {
-    background-color: red;
-  }
-  .div3 {
-    background-color: orange;
-  }
-</style>
+  
+  
 
-<!--// npm run serve -->
+</style>
